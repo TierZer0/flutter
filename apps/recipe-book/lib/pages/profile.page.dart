aa3fb7b951ee3d40636ef86fa8fe81c3937b1207
Cameron Stark <starkc920@outlook.com>
2022-12-04 14:23:10 -0600

    fix bug

--- apps/recipe-book/lib/pages/profile.page.dart
+++ apps/recipe-book/lib/pages/profile.page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -21,77 +22,95 @@ class ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    User user = authService.user;
     var theme = Theme.of(context);
     final appModel = Provider.of<AppModel>(context);
 
     return SafeArea(
       child: Material(
-        child: Container(
-          padding: const EdgeInsets.only(
-            top: 20.0,
-          ),
-          color: theme.scaffoldBackgroundColor,
-          height: MediaQuery.of(context).size.height - 90.0,
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              CustomText(
-                text: user.displayName ?? user.uid,
-                fontSize: 35.0,
-                padding: const EdgeInsets.only(left: 30.0),
-                fontFamily: "Lato",
-                color: (theme.textTheme.titleLarge?.color)!,
+          child: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+        future: userService.getUser,
+        builder: (_, snapshot) {
+          if (snapshot.hasData) {
+            var data = snapshot.data!.data();
+            return Container(
+              padding: const EdgeInsets.only(
+                top: 20.0,
               ),
-              Row(
+              color: theme.scaffoldBackgroundColor,
+              height: MediaQuery.of(context).size.height - 90.0,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   CustomText(
-                    text: "Dark Theme",
-                    fontSize: 20.0,
+                    text: data!['name'],
+                    fontSize: 35.0,
                     padding: const EdgeInsets.only(left: 30.0),
                     fontFamily: "Lato",
                     color: (theme.textTheme.titleLarge?.color)!,
                   ),
-                  Switch(
-                    value: appModel.theme,
-                    onChanged: (value) {
-                      userService.setUserTheme(value);
-                      setState(() {
-                        appModel.theme = value;
-                      });
-                    },
+                  Row(
+                    children: [
+                      CustomText(
+                        text: "Dark Theme",
+                        fontSize: 20.0,
+                        padding: const EdgeInsets.only(left: 30.0),
+                        fontFamily: "Lato",
+                        color: (theme.textTheme.titleLarge?.color)!,
+                      ),
+                      Switch(
+                        value: appModel.theme,
+                        onChanged: (value) {
+                          userService.setUserTheme(value);
+                          setState(() {
+                            appModel.theme = value;
+                          });
+                        },
+                      ),
+                    ],
                   ),
+                  CustomButton(
+                    externalPadding:
+                        const EdgeInsets.only(right: 50.0, left: 30.0),
+                    internalPadding: const EdgeInsets.symmetric(
+                      vertical: 10.0,
+                      horizontal: 20.0,
+                    ),
+                    label: 'Logout',
+                    buttonColor: primaryColor,
+                    onTap: () {
+                      appModel.uid = '';
+                      appModel.theme = false;
+                      appModel.view = 'Home';
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Successfully Logged out'),
+                        ),
+                      );
+                    },
+                    textStyle: const TextStyle(
+                      fontSize: 20.0,
+                      color: lightThemeTextColor,
+                    ),
+                  )
                 ],
               ),
-              CustomButton(
-                externalPadding: const EdgeInsets.only(right: 50.0, left: 30.0),
-                internalPadding: const EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 20.0,
-                ),
-                label: 'Logout',
-                buttonColor: primaryColor,
-                onTap: () {
-                  appModel.uid = '';
-                  appModel.theme = false;
-                  appModel.view = 'Home';
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Successfully Logged out'),
-                    ),
-                  );
-                },
-                textStyle: const TextStyle(
-                  fontSize: 20.0,
-                  color: lightThemeTextColor,
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
+            );
+          }
+          return Container(
+            padding: const EdgeInsets.only(
+              top: 20.0,
+            ),
+            color: theme.scaffoldBackgroundColor,
+            height: MediaQuery.of(context).size.height - 90.0,
+            width: MediaQuery.of(context).size.width,
+            child: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        },
+      )),
     );
   }
 }
--- apps/recipe-book/lib/services/user.service.dart
+++ apps/recipe-book/lib/services/user.service.dart
@@ -4,19 +4,23 @@ import 'package:recipe_book/services/auth.service.dart';
 
 class UserService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
-  User user = authService.user;
   String collection = 'users';
 
   setUserTheme(bool value) {
     _db
         .collection(collection)
-        .doc(user.uid)
+        .doc(authService.user.uid)
         .set({'darkTheme': value}, SetOptions(merge: true));
   }
 
   get getUserTheme async {
-    var snapshot = await _db.collection(collection).doc(user.uid).get();
+    var snapshot =
+        await _db.collection(collection).doc(authService.user.uid).get();
     return snapshot['darkTheme'];
+  }
+
+  get getUser {
+    return _db.collection(collection).doc(authService.user.uid).get();
   }
 }
 
