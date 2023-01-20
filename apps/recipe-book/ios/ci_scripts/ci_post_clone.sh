639898bbe40862f4423aab455653dd0db4f3a2e9
Cameron Stark <starkc920@outlook.com>
2023-01-20 09:10:09 -0600

    Create ci_post_clone.sh

---
+++ apps/recipe-book/ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cd apps/recipe-book
+
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
+
+flutter precache --ios
+
+flutter pub get
+
+HOMEBREW_NO_AUTO_UPDATE=1
+
+brew install cocoapods
+
+cd ios && pod install
+
+exit 0
